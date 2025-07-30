00240533c26e4acd3c129f1107a97202207401ee
mahim <mahimnxt23@gmail.com>
2025-07-30 13:48:51 +0600

    modulated stylesheets, set gradient bg

---
+++ js/gradientBG.js
@@ -0,0 +1,51 @@
+const randomX = random(-400, 400);
+const randomY = random(-200, 200);
+const randomDelay = random(0, 50);
+const randomTime = random(20, 40);
+const randomTime2 = random(5, 12);
+const randomAngle = random(-30, 150);
+
+const blurs = gsap.utils.toArray(".blur");
+blurs.forEach((blur) => {
+  gsap.set(blur, {
+    x: randomX(-1),
+    y: randomX(1),
+    rotation: randomAngle(-1),
+  });
+
+  moveX(blur, 1);
+  moveY(blur, -1);
+  rotate(blur, 1);
+});
+
+function rotate(target, direction) {
+  gsap.to(target, randomTime2(), {
+    rotation: randomAngle(direction),
+    ease: Sine.easeInOut,
+    onComplete: rotate,
+    onCompleteParams: [target, direction * -1],
+  });
+}
+
+function moveX(target, direction) {
+  gsap.to(target, randomTime(), {
+    x: randomX(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveX,
+    onCompleteParams: [target, direction * -1],
+  });
+}
+
+function moveY(target, direction) {
+  gsap.to(target, randomTime(), {
+    y: randomY(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveY,
+    onCompleteParams: [target, direction * -1],
+  });
+}
+
+function random(min, max) {
+  const delta = max - min;
+  return (direction = 1) => (min + delta * Math.random()) * direction;
+}
